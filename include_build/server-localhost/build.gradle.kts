8474bda30c3c511b9fe62d4e60f25330ecb65532
avdim <avdim@mail.ru>
2020-09-07 21:57:37 +0300

    kotlin 1.4.0, ktor 1.3.2

--- include_build/server-localhost/build.gradle.kts
+++ include_build/server-localhost/build.gradle.kts
@@ -31,10 +31,9 @@ repositories {
 
 dependencies {
   implementation(kotlin("stdlib-jdk8"))
-//  val KTOR_VERSION = "1.4.0"
-//  val COROUTINES_VERSION = "1.3.9"
   val KTOR_VERSION = "1.3.2"
-  val COROUTINES_VERSION = "1.3.7"
+//  val KTOR_VERSION = "1.4.0"
+  val COROUTINES_VERSION = "1.3.9"
 
   implementation(if (true) "ch.qos.logback:logback-classic:1.2.3" else "org.slf4j:slf4j-simple:1.7.28")
   implementation("io.ktor:ktor-server-netty:$KTOR_VERSION")
