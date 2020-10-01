da5c3e3047fcb8c6da967fdb1cec2fe922e0ce09
avdim <avdim@mail.ru>
2020-10-01 20:35:11 +0300

    version 1.0.8

--- include_build/plugin-github-token/build.gradle.kts
+++ include_build/plugin-github-token/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 val PLUGIN_NAME = "GitHub token plugin"
 val PLUGIN_ID = "ru.tutu.github.token"
-val VERSION = "1.0.7"
+val VERSION = "1.0.8"
 val TAGS = listOf("github", "token")
 
 group = "ru.tutu"
