d867075b58ec0bea2a1b77e6c9869c7c8386b941
Oleg Fedyakin <acid.lynx@ya.ru>
2022-11-08 17:58:05 +0300

    Version 1.3.4

--- include_build/plugin-github-token/build.gradle.kts
+++ include_build/plugin-github-token/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 val PLUGIN_NAME = "GitHub token plugin"
 val PLUGIN_ID = "ru.tutu.github.token"
-val VERSION = "1.3.3"
+val VERSION = "1.3.4"
 val TAGS = listOf("github", "token")
 
 group = "ru.tutu"
